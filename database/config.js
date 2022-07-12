d2af5361cedc42088a96d615f5c67083b5068e98
Mateo De Benedictis <mateodebenedictismazza@gmail.com>
2022-07-12 16:20:38 -0300

    DB Connection corrected

--- database/config.js
+++ database/config.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN);
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true, 
+            useCreateIndex: true
+        });
         console.log('DB is connected');
     } catch (error) {
         console.log(error);
